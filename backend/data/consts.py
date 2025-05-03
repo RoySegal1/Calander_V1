50f76617715ea1248c608c3fc2e4cfabe8fa567b
Roy <segalroi22@gmail.com>
2025-05-03 18:13:33 +0300

    change course credit to be string

--- backend/data/consts.py
+++ backend/data/consts.py
@@ -22,13 +22,13 @@ DEPARTMENT_CREDITS = {
 
 
 COURSES_FROM_DIFFERENT_YEARS = {
-    "10006": {"courseCredit": 5, "courseType": "חובה"},
-    "10007": {"courseCredit": 3, "courseType": "חובה"},
-    "10118": {"courseCredit": 3, "courseType": "חובה"},
-    "10124": {"courseCredit": 3.5, "courseType": "חובה"},
-    "80946": {"courseCredit": 2, "courseType": "רוח"},
-    "90913": {"courseCredit": 3, "courseType": "חובה"},
-    "10215": {"courseCredit": 4, "courseType": "חובה"},
-    "10244": {"courseCredit": 4, "courseType": "חובה"},
-    "88006": {"courseCredit": 2, "courseType": "רוח"},
+    "10006": {"courseCredit": "5", "courseType": "חובה"},
+    "10007": {"courseCredit": "3", "courseType": "חובה"},
+    "10118": {"courseCredit": "3", "courseType": "חובה"},
+    "10124": {"courseCredit": "3.5", "courseType": "חובה"},
+    "80946": {"courseCredit": "2", "courseType": "רוח"},
+    "90913": {"courseCredit": "3", "courseType": "חובה"},
+    "10215": {"courseCredit": "4", "courseType": "חובה"},
+    "10244": {"courseCredit": "4", "courseType": "חובה"},
+    "88006": {"courseCredit": "2", "courseType": "רוח"},
 }
