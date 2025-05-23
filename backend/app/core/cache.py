a9b98f0b250865324808445dcb55fa6d74ceb0f1
Roy <segalroi22@gmail.com>
2025-05-23 16:10:47 +0300

    refactor caching the courses inside cache.py

--- backend/app/core/cache.py
+++ backend/app/core/cache.py
@@ -1,2 +1,25 @@
 # This will hold the in-memory cache
+from sqlalchemy.orm import Session
+from backend.app.db.db import SessionLocal
+from backend.app.db.models import DepartmentCourses
+from backend.app.core.logger import logger
+import json
+
 global_courses_cache = {}
+
+
+def load_courses_to_mem():
+    db: Session = SessionLocal()
+    try:
+        departments = db.query(DepartmentCourses).all()
+        for dept in departments:
+            if isinstance(dept.data, str):
+                course_data = json.loads(dept.data)
+            else:
+                course_data = dept.data
+
+            global_courses_cache[dept.department_name] = course_data
+
+        logger.info("Cached %d departments at startup", len(global_courses_cache))
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,36 +1,19 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from backend.app.db.db import SessionLocal
-from backend.app.core.cache import global_courses_cache
-from backend.app.db.models import DepartmentCourses
 from backend.app.api.auth import router as auth_router
 from backend.app.api.coursesInfo import router as courses_info_router
 from backend.app.core.logger import logger
+from backend.app.core.cache import load_courses_to_mem
 import json
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # --- Startup code here ---
-    db: Session = SessionLocal()
-    try:
-        departments = db.query(DepartmentCourses).all()
-        for dept in departments:
-            if isinstance(dept.data, str):
-                course_data = json.loads(dept.data)
-            else:
-                course_data = dept.data
-
-            global_courses_cache[dept.department_name] = course_data
-
-        logger.info("Cached %d departments at startup", len(global_courses_cache))
-    finally:
-        db.close()
-
+    load_courses_to_mem()
+    logger.info("Startup tasks completed.")
     yield  # Application runs here
-
     # --- Shutdown code here (if needed) ---
     # e.g., cleanup logic, closing connections, etc.
 
