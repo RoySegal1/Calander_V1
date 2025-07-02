83e9951582891b05f366e295948d2aaa88158efa
Roy <segalroi22@gmail.com>
2025-07-02 21:56:24 +0300

    added allow origins only from site or locally to debug.

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,9 +27,9 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://www.happy-schedule.fun","http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
